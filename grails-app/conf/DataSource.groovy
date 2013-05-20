cd9092c592a2e4bf1ee82d84cf6514a621924561
Stefan Armbruster <stefan@armbruster-it.de>
2013-05-20 13:07:46 +0200

    adopting initial_hosts

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -76,7 +76,7 @@ grails {
         // location = "data/neo4j"
          params = [
                  'ha.server_id': 1,
-                 'ha.initial_hosts': '127.0.0.1:5001',
+                 'ha.initial_hosts': '127.0.0.1:5001,127.0.0.1:5002,127.0.0.1:5003',
                  'ha.cluster_server': '127.0.0.1:5001'
                  //    ha.coordinators = " localhost:2181,localhost:2182,localhost:2183"
          ]
